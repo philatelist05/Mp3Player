1f38888c95d7247294de16ce77f562e009ebda0f
Stefan Gamerith <steff7@gmx.com>
2011-12-01 17:21:18 +0100

    changed to maven build-tool

---
+++ mp3player/src/main/java/at/ac/tuwien/sepm2011ws/mp3player/App.java
@@ -0,0 +1,13 @@
+package at.ac.tuwien.sepm2011ws.mp3player;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
