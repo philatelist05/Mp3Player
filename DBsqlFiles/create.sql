2f97281b3d2a4ddbf091f8d52bbc130489622960
Klaus Krapfenbauer <klaus.krapfenbauer@gmx.com>
2011-12-11 17:53:07 +0100

    Changed types in create.sql and correct escaped file paths in insert.sql

--- DBsqlFiles/create.sql
+++ DBsqlFiles/create.sql
@@ -2,15 +2,15 @@
 CREATE SEQUENCE song_sequence
   START 1 INCREMENT BY 1;
 CREATE TABLE Song (
-  ID		NUMERIC(8)	PRIMARY KEY DEFAULT nextval('song_sequence'),
-  title		VARCHAR(100)	NOT NULL,
-  duration      NUMERIC(8)	NOT NULL,
-  rating 	NUMERIC(3)	NOT NULL,
-  playcount	NUMERIC(8)	NOT NULL,
-  year		DATE		NOT NULL,
-  path  	VARCHAR(100)	NOT NULL,
-  artist  	VARCHAR(100)	NOT NULL,
-  genre		VARCHAR(30)	NOT NULL,
+  ID		INTEGER		PRIMARY KEY DEFAULT nextval('song_sequence'),
+  title		VARCHAR(255)	NOT NULL,
+  duration      INTEGER	NOT NULL,
+  rating 	NUMERIC(2)	NOT NULL,
+  playcount	INTEGER		NOT NULL,
+  year		INTEGER		NOT NULL,
+  path  	VARCHAR(512)	NOT NULL,
+  artist  	VARCHAR(255)	NOT NULL,
+  genre		VARCHAR(255)	NOT NULL,
   CHECK (rating BETWEEN 0 AND 10)
 );
 ALTER SEQUENCE song_sequence OWNED BY Song.ID;
@@ -19,10 +19,10 @@ ALTER SEQUENCE song_sequence OWNED BY Song.ID;
 CREATE SEQUENCE album_sequence
   START 1 INCREMENT BY 1;
 CREATE TABLE Album (
-  ID		NUMERIC(8)	PRIMARY KEY DEFAULT nextval('album_sequence'),
-  title		VARCHAR(100)	NOT NULL,
-  year		DATE		NOT NULL,
-  albumart_path	VARCHAR(100)	NOT NULL
+  ID		INTEGER	PRIMARY KEY DEFAULT nextval('album_sequence'),
+  title		VARCHAR(255)	NOT NULL,
+  year		INTEGER		NOT NULL,
+  albumart_path	VARCHAR(512)	NOT NULL
 );
 ALTER SEQUENCE album_sequence OWNED BY Album.ID;
 
@@ -30,19 +30,19 @@ ALTER SEQUENCE album_sequence OWNED BY Album.ID;
 CREATE SEQUENCE playlist_sequence
   START 1 INCREMENT BY 1;
 CREATE TABLE Playlist (
-  ID		NUMERIC(8)	PRIMARY KEY DEFAULT nextval('playlist_sequence'),
-  name		VARCHAR(100)	NOT NULL
+  ID		INTEGER	PRIMARY KEY DEFAULT nextval('playlist_sequence'),
+  name		VARCHAR(255)	NOT NULL
 );
 ALTER SEQUENCE playlist_sequence OWNED BY Playlist.ID;
 
 -- is_on (album:Album.ID, song:Song.ID)
 CREATE TABLE is_on (
-  album		NUMERIC(8)	REFERENCES Album(ID),
-  song		NUMERIC(8)	REFERENCES Song(ID)
+  album		INTEGER	REFERENCES Album(ID),
+  song		INTEGER	REFERENCES Song(ID)
 );
 
 -- contains (playlist:Playlist.ID, song:Song.ID)
 CREATE TABLE contains (
-  playlist	NUMERIC(8)	REFERENCES Playlist(ID),
-  song		NUMERIC(8)	REFERENCES Song(ID)
+  playlist	INTEGER	REFERENCES Playlist(ID),
+  song		INTEGER	REFERENCES Song(ID)
 );
--- DBsqlFiles/insert.sql
+++ DBsqlFiles/insert.sql
@@ -2,10 +2,10 @@ INSERT INTO Playlist VALUES
   (DEFAULT,  'Metääälll');
 
 INSERT INTO Album VALUES
-  (DEFAULT,  'Black Album', '1995-01-03', 'C:\music\Metallica');
+  (DEFAULT,  'Black Album', 1995, E'C:\\music\\Metallica');
 
 INSERT INTO Song VALUES
-  (DEFAULT,  'Enter Sandman', 184, 9, 34, '1995-01-03', 'C:\music\Metallica\entersandman', 'Metallica', 'Metal');
+  (DEFAULT,  'Enter Sandman', 184, 9, 34, 1995, E'C:\\music\\Metallica\\entersandman', 'Metallica', 'Metal');
 INSERT INTO is_on VALUES
   (currval(pg_get_serial_sequence('Album', 'id')),  currval(pg_get_serial_sequence('Song', 'id')));
 
@@ -13,7 +13,7 @@ INSERT INTO contains VALUES
   (currval(pg_get_serial_sequence('Playlist', 'id')),  currval(pg_get_serial_sequence('Song', 'id')));  
   
 INSERT INTO Song VALUES
-  (DEFAULT,  'Wherever I May Roam', 133, 8, 77, '1995-01-03', 'C:\music\Metallica\whereverimayroam', 'Metallica', 'Metal');
+  (DEFAULT,  'Wherever I May Roam', 133, 8, 77, 1995, E'C:\\music\\Metallica\\whereverimayroam', 'Metallica', 'Metal');
 INSERT INTO is_on VALUES
   (currval(pg_get_serial_sequence('Album', 'id')),  currval(pg_get_serial_sequence('Song', 'id')));
   
@@ -27,9 +27,9 @@ INSERT INTO contains VALUES
   (currval(pg_get_serial_sequence('Playlist', 'id')),  currval(pg_get_serial_sequence('Song', 'id')));
 
 INSERT INTO Album VALUES
-  (DEFAULT,  'Panic Prevention', '2001-05-03', 'C:\music\JamieT');  
+  (DEFAULT,  'Panic Prevention', 2001, E'C:\\music\\JamieT');  
 INSERT INTO Song VALUES
-  (DEFAULT,  'Sticks n Stones', 213, 5, 12, '2001-05-03', 'C:\music\JamieT\sticksnstones', 'JamieT', 'Folk, Ska, Electronic');
+  (DEFAULT,  'Sticks n Stones', 213, 5, 12, 2001, E'C:\\music\\JamieT\\sticksnstones', 'JamieT', 'Folk, Ska, Electronic');
 INSERT INTO is_on VALUES
   (currval(pg_get_serial_sequence('Album', 'id')),  currval(pg_get_serial_sequence('Song', 'id')));
   
@@ -38,13 +38,13 @@ INSERT INTO contains VALUES
 
   
 INSERT INTO Song VALUES
-  (DEFAULT,  'Nosebleed Secton', 678, 9, 34, '2003-01-03', 'C:\music\HilltopHoods\nosebleedsection', 'Hilltop Hoods', 'Rap');
+  (DEFAULT,  'Nosebleed Secton', 678, 9, 34, 2003, E'C:\\music\\HilltopHoods\\nosebleedsection', 'Hilltop Hoods', 'Rap');
  
 INSERT INTO contains VALUES
   (currval(pg_get_serial_sequence('Playlist', 'id')),  currval(pg_get_serial_sequence('Song', 'id')));
   
 INSERT INTO Song VALUES  
-  (DEFAULT,  'I Kissed a Girl', 302, 5, 12, '2007-01-03', 'C:\music\KatyPerry\ikissedagirl', 'Katy Perry', 'Pop');
+  (DEFAULT,  'I Kissed a Girl', 302, 5, 12, 2007, E'C:\\music\\KatyPerry\\ikissedagirl', 'Katy Perry', 'Pop');
   
 INSERT INTO contains VALUES
   (currval(pg_get_serial_sequence('Playlist', 'id')),  currval(pg_get_serial_sequence('Song', 'id')));
